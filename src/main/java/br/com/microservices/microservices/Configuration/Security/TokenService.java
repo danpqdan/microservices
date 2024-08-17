7e0f7de059faaae3005ba9102bc513241d69efad
Daniel J. Santos <danieltisantos@gmail.com>
2024-08-17 18:39:34 -0300

    Att var de amb que continha erro

--- src/main/java/br/com/microservices/microservices/Configuration/Security/TokenService.java
+++ src/main/java/br/com/microservices/microservices/Configuration/Security/TokenService.java
@@ -12,7 +12,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 
-import br.com.microservices.microservices.Model.User;
+import br.com.microservices.microservices.Model.Users;
 
 @Service
 public class TokenService {
@@ -20,11 +20,11 @@ public class TokenService {
     @Value("${api.security.token.secret}")
     private String secret;
 
-    public String generatedToken(User user) {
+    public String generatedToken(Users user) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
             String token = JWT.create()
-                    .withIssuer("auth.api")
+                    .withIssuer("microservices")
                     .withSubject(user.getUsername())
                     .withExpiresAt(genExpirationDate())
                     .sign(algorithm);
@@ -38,12 +38,12 @@ public class TokenService {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.require(algorithm)
-            .withIssuer("auth-api")
+            .withIssuer("microservices")
             .build()
             .verify(token)
             .getSubject();
         } catch (JWTVerificationException e) {
-            return "";
+            return null;
         }
     }
 
